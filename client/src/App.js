8d196a87836fda179b828b6a7d86634b162807c6
Lee Presswood <LeePresswood@gmail.com>
2019-02-28 19:50:39 -0600

    Added client with beginner React call.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = { loaded: [] };
+  }
+
+  componentWillMount() {
+    fetch('http://localhost:8080/connection-info?ip=www.google.com')
+      .then(res => res.json())
+      .then(whois => {
+        this.setState({ ...this.state, loaded: [whois] });
+      })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App" >
+        <div className="col"></div>
+        <div className="col"></div>
       </div>
     );
   }
--- server/src/main/java/com/leepresswood/adaware/InfoController.java
+++ server/src/main/java/com/leepresswood/adaware/InfoController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,31 +20,31 @@ public class InfoController {
     @Autowired
     private InfoService service;
 
+    @CrossOrigin
     @GetMapping(value = "/connection-info", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Whois> getConnectionInfo(
-            @RequestParam(value = "ip", defaultValue = "") String ip) throws InterruptedException, ExecutionException {
+    public ResponseEntity<Whois> getConnectionInfo(@RequestParam(value = "ip", defaultValue = "") String ip)
+            throws InterruptedException, ExecutionException {
         if (ip == null || ip.length() == 0) {
             throw new InvalidParameterException();
         }
 
         Whois response = service.getIpInfo(ip);
 
-        if(response == null){
+        if (response == null) {
             throw new InvalidParameterException();
         }
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-
-    @ExceptionHandler({InvalidParameterException.class})
+    @ExceptionHandler({ InvalidParameterException.class })
     public ResponseEntity<Map> userException() {
         return new ResponseEntity<>(Collections.singletonMap("reason", "Bad Request"), HttpStatus.BAD_REQUEST);
     }
 
-
-    @ExceptionHandler({InterruptedException.class, ExecutionException.class})
+    @ExceptionHandler({ InterruptedException.class, ExecutionException.class })
     public ResponseEntity<Map> serverException() {
-        return new ResponseEntity<>(Collections.singletonMap("reason", "Server Error"), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(Collections.singletonMap("reason", "Server Error"),
+                HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }

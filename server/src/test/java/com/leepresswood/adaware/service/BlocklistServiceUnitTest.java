16fadb326b6ce836d0720a8a246514bc97410b35
Lee Presswood <LeePresswood@gmail.com>
2019-03-04 09:55:59 -0600

    Finished service testing.

--- server/src/test/java/com/leepresswood/adaware/service/BlocklistServiceUnitTest.java
+++ server/src/test/java/com/leepresswood/adaware/service/BlocklistServiceUnitTest.java
@@ -25,14 +25,14 @@ public class BlocklistServiceUnitTest {
     @InjectMocks
     BlocklistService service;
 
+    List<Blocklist> list = Lists.newArrayList(
+            new Blocklist(1),
+            new Blocklist(2),
+            new Blocklist(3)
+    );
+
     @Test
     public void shouldReturnFullList() {
-        List<Blocklist> list = Lists.newArrayList(
-                new Blocklist(1),
-                new Blocklist(2),
-                new Blocklist(3)
-        );
-
         MockitoAnnotations.initMocks(this);
         service = new BlocklistService();
         service.repo = repo;
@@ -53,5 +53,197 @@ public class BlocklistServiceUnitTest {
 
         Assertions.assertThat(response.get(2).id)
                   .isEqualTo(3);
+    }
+
+    @Test
+    public void shouldReturnPartialListByStart() {
+        MockitoAnnotations.initMocks(this);
+        service = new BlocklistService();
+        service.repo = repo;
+
+        Mockito.when(repo.findAll())
+               .thenReturn(list);
+
+        List<Blocklist> response = service.getAllBlockedIps(2, -1);
+
+        Assertions.assertThat(response.size())
+                  .isEqualTo(2);
+
+        Assertions.assertThat(response.get(0).id)
+                  .isEqualTo(2);
+
+        Assertions.assertThat(response.get(1).id)
+                  .isEqualTo(3);
+    }
+
+    @Test
+    public void shouldReturnPartialListBySize() {
+        MockitoAnnotations.initMocks(this);
+        service = new BlocklistService();
+        service.repo = repo;
+
+        Mockito.when(repo.findAll())
+               .thenReturn(list);
+
+        List<Blocklist> response = service.getAllBlockedIps(-1, 2);
+
+        Assertions.assertThat(response.size())
+                  .isEqualTo(2);
+
+        Assertions.assertThat(response.get(0).id)
+                  .isEqualTo(1);
+
+        Assertions.assertThat(response.get(1).id)
+                  .isEqualTo(2);
+    }
+
+    @Test
+    public void shouldReturnPartialListByStartAndSize() {
+        MockitoAnnotations.initMocks(this);
+        service = new BlocklistService();
+        service.repo = repo;
+
+        Mockito.when(repo.findAll())
+               .thenReturn(list);
+
+        List<Blocklist> response = service.getAllBlockedIps(2, 1);
+
+        Assertions.assertThat(response.size())
+                  .isEqualTo(1);
+
+        Assertions.assertThat(response.get(0).id)
+                  .isEqualTo(2);
+    }
+
+    @Test
+    public void shouldReturnPartialListByStartAndSizeOutsideArray() {
+        MockitoAnnotations.initMocks(this);
+        service = new BlocklistService();
+        service.repo = repo;
+
+        Mockito.when(repo.findAll())
+               .thenReturn(list);
+
+        List<Blocklist> response = service.getAllBlockedIps(2, 10);
+
+        Assertions.assertThat(response.size())
+                  .isEqualTo(2);
+
+        Assertions.assertThat(response.get(0).id)
+                  .isEqualTo(2);
+
+        Assertions.assertThat(response.get(1).id)
+                  .isEqualTo(3);
+    }
+
+    @Test
+    public void shouldReturnPartialListByStartOutsideArray() {
+       MockitoAnnotations.initMocks(this);
+        service = new BlocklistService();
+        service.repo = repo;
+
+        Mockito.when(repo.findAll())
+               .thenReturn(list);
+
+        List<Blocklist> response = service.getAllBlockedIps(-100, -1);
+
+        Assertions.assertThat(response.size())
+                  .isEqualTo(3);
+
+        Assertions.assertThat(response.get(0).id)
+                  .isEqualTo(1);
+
+        Assertions.assertThat(response.get(1).id)
+                  .isEqualTo(2);
+
+        Assertions.assertThat(response.get(2).id)
+                  .isEqualTo(3);
+    }
+
+    @Test
+    public void shouldReturnPartialListByStartOutsideArrayAndPositive() {
+        MockitoAnnotations.initMocks(this);
+        service = new BlocklistService();
+        service.repo = repo;
+
+        Mockito.when(repo.findAll())
+               .thenReturn(list);
+
+        List<Blocklist> response = service.getAllBlockedIps(100, -1);
+
+        Assertions.assertThat(response.size())
+                  .isEqualTo(3);
+
+        Assertions.assertThat(response.get(0).id)
+                  .isEqualTo(1);
+
+        Assertions.assertThat(response.get(1).id)
+                  .isEqualTo(2);
+
+        Assertions.assertThat(response.get(2).id)
+                  .isEqualTo(3);
+    }
+
+    @Test
+    public void shouldReturnPartialListByStartAndSizeBothOutsideArrayAndPositive() {
+        MockitoAnnotations.initMocks(this);
+        service = new BlocklistService();
+        service.repo = repo;
+
+        Mockito.when(repo.findAll())
+               .thenReturn(list);
+
+        List<Blocklist> response = service.getAllBlockedIps(100, 10);
+
+        Assertions.assertThat(response.size())
+                  .isEqualTo(3);
+
+        Assertions.assertThat(response.get(0).id)
+                  .isEqualTo(1);
+
+        Assertions.assertThat(response.get(1).id)
+                  .isEqualTo(2);
+
+        Assertions.assertThat(response.get(2).id)
+                  .isEqualTo(3);
+    }
+
+    @Test
+    public void shouldReturnPartialListBy0Start() {
+        MockitoAnnotations.initMocks(this);
+        service = new BlocklistService();
+        service.repo = repo;
+
+        Mockito.when(repo.findAll())
+               .thenReturn(list);
+
+        List<Blocklist> response = service.getAllBlockedIps(0, -1);
+
+        Assertions.assertThat(response.size())
+                  .isEqualTo(3);
+
+        Assertions.assertThat(response.get(0).id)
+                  .isEqualTo(1);
+
+        Assertions.assertThat(response.get(1).id)
+                  .isEqualTo(2);
+
+        Assertions.assertThat(response.get(2).id)
+                  .isEqualTo(3);
+    }
+
+    @Test
+    public void shouldReturnPartialListBy0Size() {
+        MockitoAnnotations.initMocks(this);
+        service = new BlocklistService();
+        service.repo = repo;
+
+        Mockito.when(repo.findAll())
+               .thenReturn(list);
+
+        List<Blocklist> response = service.getAllBlockedIps(-1, 0);
+
+        Assertions.assertThat(response.size())
+                  .isEqualTo(0);
     }
 }
